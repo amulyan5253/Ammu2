aae867e86ea166ffd73af6d120c91b344547e88b
amulyan5253 <amulya.py5055@gmail.com>
2022-03-15 13:41:55 -0700

    Initial Commit

---
+++ main.py
@@ -0,0 +1,19 @@
+myfile = 'numbers.txt'
+sum = 0
+even = 0
+odd = 0
+with open(myfile) as fh:
+    for line in fh:
+        numbers = line.strip().split(',')
+        for ns in numbers:
+            sum += int(ns)
+            if(int(ns) % 2 == 0):
+                even += int(ns)
+            else:
+                odd += int(ns)
+print(sum)
+print(even)
+print(odd)
+
+
+
